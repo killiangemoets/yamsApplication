4ea34a7c93ce4db76822b0fa7180aaad7c489da7
Killian Gemoets <kilgem@live.fr>
2022-12-06 21:40:59 +0100

    package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
 // ** MIDDLEWARE ** //
 // const whitelist = [
@@ -30,6 +31,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
